51de44c8d4b23c940a241b032e355f6704644bb8
tysonfoodsreporting <tysonfoods_reporting@mediabymother.com>
2025-07-25 13:02:59 +0530

    Update stg_pinterest_ads__pin_promotion_targeting_report.sql

--- models/stg_pinterest_ads__pin_promotion_targeting_report.sql
+++ models/stg_pinterest_ads__pin_promotion_targeting_report.sql
@@ -1,4 +1,11 @@
-{{ config(enabled=fivetran_utils.enabled_vars(['ad_reporting__pinterest_ads_enabled','pinterest__using_pin_promotion_targeting_report'])) }}
+{{ config(enabled=var('ad_reporting__pinterest_ads_enabled', True),
+    unique_key = ['source_relation','date_day','pin_promotion_id','ad_group_id','campaign_id','advertiser_id','targeting_type'],
+    partition_by={
+      "field": "date_day", 
+      "data_type": "TIMESTAMP",
+      "granularity": "day"
+    }
+    ) }}
 
 with base as (
 
