6e37d5e5d509d3c548b1e1fd7a98cb1d173edf35
tysonfoodsreporting <tysonfoods_reporting@mediabymother.com>
2025-07-25 12:58:46 +0530

    Update stg_pinterest_ads__keyword_report.sql

--- models/stg_pinterest_ads__keyword_report.sql
+++ models/stg_pinterest_ads__keyword_report.sql
@@ -1,4 +1,11 @@
-{{ config(enabled=fivetran_utils.enabled_vars(['ad_reporting__pinterest_ads_enabled','pinterest__using_keywords'])) }}
+{{ config(enabled=fivetran_utils.enabled_vars(['ad_reporting__pinterest_ads_enabled','pinterest__using_keywords']),
+    unique_key = ['source_relation','date_day','keyword_id','pin_promotion_id','ad_group_id','campaign_id','advertiser_id'],
+    partition_by={
+      "field": "date_day", 
+      "data_type": "TIMESTAMP",
+      "granularity": "day"
+    }
+    ) }}
 
 with base as (
 
