ba3a054fdbbbde2313d8e898e0a0c3cabe13e34f
tysonfoodsreporting <tysonfoods_reporting@mediabymother.com>
2025-08-06 17:45:54 +0530

    Update stg_pinterest_ads__pin_promotion_report.sql

--- models/stg_pinterest_ads__pin_promotion_report.sql
+++ models/stg_pinterest_ads__pin_promotion_report.sql
@@ -35,7 +35,7 @@ final as (
 
     select
         source_relation, 
-        TIMESTAMP(DATETIME(TIMESTAMP(date), "America/Chicago")) as date_day,
+        date(DATETIME(TIMESTAMP(date), "America/Chicago")) as date_day,
         cast(pin_promotion_id as {{ dbt.type_string() }}) as pin_promotion_id,
         cast(ad_group_id as {{ dbt.type_string() }}) as ad_group_id,
         cast(campaign_id as {{ dbt.type_string() }}) as campaign_id,
